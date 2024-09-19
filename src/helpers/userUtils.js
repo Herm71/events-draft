56f55bcd70f613466e4f8cb2de2e152902791006
Jason Chafin <jchafin@ucsc.edu>
2024-09-19 06:07:11 -0700

    Edit `userUtils.js`

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -1,7 +1,13 @@
 // Put your computations here.
 
 function userComputed(data) {
-  return {};
+  return {
+		first_name: 'Jason',
+		last_name: 'Chafin',
+		title: 'Senior Web Developer, C&M Web Team',
+		dept: 'Communications &amp; Marketing',
+		div: 'University Advancement'
+	};
 }
 
 exports.userComputed = userComputed;
